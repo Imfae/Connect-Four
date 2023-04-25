b62c965f763647034641e925aa8cc80ef1cadaf8
Imfae <95619108+Imfae@users.noreply.github.com>
2023-04-25 22:55:08 +0800

    Implement replay option

--- lib/connect-four.rb
+++ lib/connect-four.rb
@@ -94,5 +94,22 @@ class Game
   end
 end
 
-game = Game.new
-game.gameplay
+def play_again?
+  play_again = $stdin.gets.chomp.downcase
+  if play_again.include?('y')
+    true
+  elsif play_again.include?('n')
+    false
+  else
+    puts "\nPlease enter 'y' or 'n'."
+    play_again?
+  end
+end
+
+## Game loop
+loop do
+  game = Game.new
+  game.gameplay
+  puts "\nWould you like to play again? (y/n)"
+  play_again? ? redo : break
+end
